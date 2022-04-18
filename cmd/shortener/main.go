e8285c3edb94dd5b3eb3388920ecf3f8b2d59319
Alexandr Kvasnikov <videot4pe@gmail.com>
2022-04-18 12:59:23 +0300

    feat: inc-1 server & routes

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"github.com/truthray/url-shortening-service/internal/app/server"
+)
+
+func main() {
+	s := server.New()
+
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ internal/app/server/server.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Server struct {
+	Addr string
+}
+
+func New() *Server {
+	return &Server{
+		Addr: "localhost:8080",
+	}
+}
+
+func (s *Server) Start() error {
+	http.Handle("/", s.handleRequest())
+	return http.ListenAndServe(s.Addr, nil)
+}
+
+func (s *Server) handleRequest() http.HandlerFunc {
+	data := make(map[int]string)
+	counter := 0
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			params := strings.Split(r.URL.Path, "/")
+			if len(params) < 2 {
+				http.Error(w, "Query parameter is missing", http.StatusBadRequest)
+				return
+			}
+			code, err := strconv.Atoi(params[len(params)-1])
+			if err != nil {
+				http.Error(w, "Query parameter is not integer", http.StatusBadRequest)
+				return
+			}
+			if url, ok := data[code]; ok {
+				w.Header().Set("Location", url)
+				w.WriteHeader(http.StatusTemporaryRedirect)
+				return
+			}
+			http.Error(w, "Id not found", http.StatusNotFound)
+
+		case http.MethodPost:
+			b, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Body is missing", http.StatusBadRequest)
+				return
+			}
+			data[counter] = string(b)
+			w.WriteHeader(http.StatusCreated)
+			fmt.Fprint(w, s.Addr, "/", counter)
+			counter += 1
+		default:
+			http.Error(w, "Only GET or POST requests are allowed", http.StatusMethodNotAllowed)
+		}
+	}
+}
